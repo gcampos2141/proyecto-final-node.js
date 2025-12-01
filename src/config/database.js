959ed599724c32d2095b21a32b04594331d047a8
Gabriel Campos <gcampos2141@gmail.com>
2025-11-30 21:06:36 -0600

    Conexión DB CleverCloud

--- src/config/database.js
+++ src/config/database.js
@@ -5,7 +5,10 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'taller_node_db'
+  database: process.env.DB_NAME || 'taller_node_db',
+
+  connectionLimit: 5, 
+  idleTimeout: 60000
 });
 
 pool.query = util.promisify(pool.query);
